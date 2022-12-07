d4b674ea7e6cf619cef006f9aa21c79c0379f412
zhanzheng8585 <zhanzheng8585@gmail.com>
2022-12-07 14:58:21 -0500

    Update run_mag_prune_irr_sp_EM_0.75_gradient0.80.sh

--- scripts/resnet18/run_mag_prune_irr_sp_EM_0.75_gradient0.80.sh
+++ scripts/resnet18/run_mag_prune_irr_sp_EM_0.75_gradient0.80.sh
@@ -1,48 +1,50 @@
-cd ../../..
+# CL params
+BUFFER_SIZE=500
 
 # magnitude-based 1 shot retraining
-ARCH=${1:-"resnet"} # 
-DEPTH=${2:-"18"}
-PRUNE_ARGS=${5:-"--sp-retrain --sp-prune-before-retrain"}
-LOAD_CKPT=${6:-"XXXXX.pth.tar"}
-INIT_LR=${8:-"0.1"}
-EPOCHS=${11:-"250"}
-WARMUP=${12:-"8"}
+ARCH="resnet" # 
+DEPTH="18"
+PRUNE_ARGS="--sp-retrain --sp-prune-before-retrain"
+LOAD_CKPT="XXXXX.pth.tar"     # automatically train from scratch if the given checkpoint model is not found
+INIT_LR="0.03"
+EPOCHS="250"
+WARMUP="8"
 
-DATASET=${20:-"seq-cifar10"}
+SPARSITY_TYPE="irregular"
+DATASET="seq-cifar10"
 
-GLOBAL_BATCH_SIZE=${9:-"32"}
-MASK_UPDATE_DECAY_EPOCH=${18:-"25-40"}
-# MASK_UPDATE_DECAY_EPOCH=${18:-"90-120"}
-SP_MASK_UPDATE_FREQ=${19:-"2"}
+GLOBAL_BATCH_SIZE="32"
+MASK_UPDATE_DECAY_EPOCH="25-40"
+# MASK_UPDATE_DECAY_EPOCH="90-120"
+SP_MASK_UPDATE_FREQ="5"
 
-REMOVE_N=${20:-"2000"}
-KEEP_LOWEST_N=${21:-"0"}
+REMOVE_N=3000
+RM_EPOCH=20
 
-GPU_ID=${15:-"0"}
-SEED=${10:-"914"}
+SAVE_FOLDER="checkpoints/resnet18/paper/gradient_effi/mutate_irr/${DATASET}/buffer_${BUFFER_SIZE}/"
 
-RM_EPOCH=${23:-"20"}
-GRADIENT=${25:-"0.80"}
-
-# --------------------------------------------------------------------------
-SPARSITY_TYPE=${3:-"irregular"}
-SAVE_FOLDER=${7:-"checkpoints/resnet18/data_sparse/mutate_irr/${DATASET}/remove_${REMOVE_N}_${GRADIENT}_cl_mask/"}
+cd /home/zhan.zhe/CL_sparse_training
 
 mkdir -p ${SAVE_FOLDER}
 
-LOWER_BOUND=${17:-"0.77-0.76-0.75"}
-UPPER_BOUND=${16:-"0.75-0.75-0.75"}
+GPU_ID=0
+SEED=250
 
-CONFIG_FILE=${4:-"./profiles/resnet18_cifar/irr/resnet18_0.75.yaml"}
+GRADIENT=0.80
+# ------- for 75% overall sparsity ----------
+# ------- check retrain.py for more information ----------
+LOWER_BOUND="0.77-0.76-0.75" #
+UPPER_BOUND="0.75-0.75-0.75"
 
-REMARK=${13:-"irr_0.75_mut_RM_${REMOVE_N}"}
-LOG_NAME=${14:-"irr_0.75_mut_RM_${REMOVE_N}_${RM_EPOCH}"}
-PKL_NAME=${23:-"irr_0.75_mut_RM_${REMOVE_N}_${RM_EPOCH}"}
-BUFFER_SIZE=${24:-"500"}
+CONFIG_FILE="./profiles/resnet18_cifar/irr/resnet18_0.75.yaml"
+REMARK="irr_0.75_mut"
+LOG_NAME="75_derpp_${GRADIENT}"
+PKL_NAME="irr_0.75_mut_RM_${REMOVE_N}_${RM_EPOCH}"
+# EVAL_CHECKPOINT="./checkpoints/resnet18/mutate_irr/seed914_irr_0.75_mut_resnet18_seq-cifar10_acc_57.110_fgt_43.500_sgd_lr0.1_cosine_sp0.000_task_4.pt"
 
 
 CUDA_VISIBLE_DEVICES=${GPU_ID} python3 -u main_sparse_train_w_data_gradient_efficient.py \
-    --arch ${ARCH} --depth ${DEPTH} --optmzr sgd --batch-size ${GLOBAL_BATCH_SIZE} --lr ${INIT_LR} --lr-scheduler cosine --save-model ${SAVE_FOLDER} --epochs ${EPOCHS} --dataset ${DATASET} --seed ${SEED} --upper-bound ${UPPER_BOUND} --lower-bound ${LOWER_BOUND} --mask-update-decay-epoch ${MASK_UPDATE_DECAY_EPOCH} --sp-mask-update-freq ${SP_MASK_UPDATE_FREQ} --remark ${REMARK} ${PRUNE_ARGS} --sp-admm-sparsity-type=${SPARSITY_TYPE} --sp-config-file=${CONFIG_FILE} \
-    --log-filename=${SAVE_FOLDER}/seed_${SEED}_${LOG_NAME}.txt --output-dir ${SAVE_FOLDER} --output-name ${PKL_NAME} --remove-n ${REMOVE_N} --keep-lowest-n ${KEEP_LOWEST_N} \
-    --remove-data-epoch ${RM_EPOCH} --buffer-size ${BUFFER_SIZE} --gradient_efficient_mix --gradient_sparse ${GRADIENT}
+        --arch ${ARCH} --depth ${DEPTH} --optmzr sgd --batch-size ${GLOBAL_BATCH_SIZE} --lr ${INIT_LR} --lr-scheduler cosine --save-model ${SAVE_FOLDER} --epochs ${EPOCHS} --dataset ${DATASET} --seed ${SEED} --upper-bound ${UPPER_BOUND} --lower-bound ${LOWER_BOUND} --mask-update-decay-epoch ${MASK_UPDATE_DECAY_EPOCH} --sp-mask-update-freq ${SP_MASK_UPDATE_FREQ} --remark ${REMARK} ${PRUNE_ARGS} --sp-admm-sparsity-type=${SPARSITY_TYPE} --sp-config-file=${CONFIG_FILE} \
+        --log-filename=${SAVE_FOLDER}/seed_${SEED}_${LOG_NAME}.txt --buffer-size=$BUFFER_SIZE --replay_method derpp --buffer_weight 0.1 --buffer_weight_beta 0.5 \
+        --use_cl_mask --gradient_efficient_mix --gradient_sparse=$GRADIENT --remove-n=$REMOVE_N --keep-lowest-n 0 --remove-data-epoch=$RM_EPOCH --output-dir ${SAVE_FOLDER} --output-name=${PKL_NAME}
+        # --evaluate_mode --eval_checkpoint=${EVAL_CHECKPOINT}
